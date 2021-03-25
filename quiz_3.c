7ef9720e4acf26441ed9d98619f5f71e6c346aeb
SaThitiphon <78350405+SaThitiphon@users.noreply.github.com>
2021-03-25 17:23:34 +0700

    Update quiz_3.c

--- quiz_3.c
+++ quiz_3.c
@@ -9,7 +9,7 @@ int main()
 		{
 			count++;
 		}
-		if(i>10 && i%11==0)
+		if(i>10 && i%11==0 && i<100)
 		{
 			count++;
 		}
@@ -22,6 +22,34 @@ int main()
 				count++;
 			}
 		}
+		if(i>1000)
+		{
+			a = i % 10;
+			b = i / 1000;
+			if(a==b)
+			{
+				count++;
+			}
+		}
+		if(i>10000)
+		{
+			a = i % 10;
+			b = i / 1000;
+			if(a==b)
+			{
+				count++;
+			}
+		}
+		if(i>100000)
+		{
+			a = i % 10;
+			b = i / 100000;
+			if(a==b)
+			{
+				count++;
+			}
+		}
+		
 	}
 	printf("%d",count);
 }
