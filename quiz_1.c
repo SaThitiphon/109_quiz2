0c6ed972bb10ff19bcc1a36b79269f23e4457830
SaThitiphon <78350405+SaThitiphon@users.noreply.github.com>
2021-03-25 17:45:25 +0700

    test

--- quiz_1.c
+++ quiz_1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-	int n,i,j,m;
+	int n,i,j,m,count=0;
 	scanf("%d",&n);
-	for(i=2;i<100;i++)
+	for(i=2;i<100000;i++)
 	{
 		m = i * i * i;
 		if(n%m==0)
@@ -11,5 +11,23 @@ int main()
 			printf("Not Cube Free");
 			break;
 		}
+	}
+	for(i=1;i<100000;i++)
+	{
+		for(j=2;j<100000;j++)
+		{
+			m = j * j * j;
+			if(i%m==0)
+			{
+				count--;
+			}			
+		}
+		if(i==n)
+		{
+			count++;
+			printf("%d",count);
+			break;
+		}
+		count++;
 	}
 }
